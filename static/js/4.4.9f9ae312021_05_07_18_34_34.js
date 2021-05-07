cb025e6b60fbaf06ae4afff8a3b8a7b9b800875e
liyongshuai <liys0820@163.com>
2021-05-07 19:10:54 +0800

    fix: update

--- static/js/4.4.9f9ae312021_05_07_18_34_34.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{766:function(e,n,t){"use strict";t.r(n),t.d(n,"default",(function(){return s}));var r=t(0);class s extends r.Component{render(){return r.createElement("div",null,"data-service")}}}}]);
-//# sourceMappingURL=4.4.9f9ae312021_05_07_18_34_34.js.map
