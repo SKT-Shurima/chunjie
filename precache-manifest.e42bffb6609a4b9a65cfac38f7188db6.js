5b21ab89983b3c9ebea76a6d1e701f266d20c644
liyongshuai <liys0820@163.com>
2021-05-08 16:57:29 +0800

    fix: update

---
+++ precache-manifest.e42bffb6609a4b9a65cfac38f7188db6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bc16136925506bdd8a28fb26fa0a07a",
+    "url": "./static/img/modal_close.9bc1613.svg"
+  },
+  {
+    "revision": "938057a183bbbe2eb037188319b9bf26",
+    "url": "./static/img/no-data.938057a.svg"
+  },
+  {
+    "revision": "18da22a3f8c19a29bf38",
+    "url": "./static/js/3.3.18da22a2021_05_08_16_56_55.js"
+  },
+  {
+    "revision": "9f9ae3138a6a8eb950cf",
+    "url": "./static/js/4.4.9f9ae312021_05_08_16_56_55.js"
+  },
+  {
+    "revision": "1a5d157a06ff55266f67",
+    "url": "./static/js/app.1.1a5d1572021_05_08_16_56_55.js"
+  },
+  {
+    "revision": "5a3e60f2d4e6b317c489",
+    "url": "./static/js/manifest.5a3e60f2021_05_08_16_56_55.js"
+  },
+  {
+    "revision": "cc9464d831362cc7ba9a",
+    "url": "./static/js/vendor.0.cc9464d2021_05_08_16_56_55.js"
+  },
+  {
+    "revision": "19e6d8e57d719aefbfb9e1727efd30a0",
+    "url": "./static/js/vendor.0.cc9464d2021_05_08_16_56_55.js.LICENSE"
+  }
+]);
